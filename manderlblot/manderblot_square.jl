a7743f659bf154f351df78faea6454b2021cbf0e
nagamachia <7518545@alumni.tus.ac.jp>
2021-03-07 12:46:22 +0900

    fixed to change exponents from 2 to 5

--- manderlblot/manderblot_square.jl
+++ manderlblot/manderblot_square.jl
@@ -1,27 +1,29 @@
 using Plots
 function calc()
     imax=500
-    result=zeros(Float64,2,(imax*2+1)^2)
-    cnt=0
-    dx=2.0/imax
-    dy=2.0/imax
-    for i in -imax:imax
-        for j in -imax:imax
-            x=dx*i
-            y=dy*j
-            c=complex(x,y)
-            z=0.0im
-            flag=0
-            for l in 1:200
-                z=z^2+c
-                if abs(z)>2.0
-                    flag=1
-                    break
+    result=zeros(Float64,5,2,(imax*2+1)^2)
+    for iter in 2:5
+        cnt=0
+        dx=2.0/imax
+        dy=2.0/imax
+        for i in -imax:imax
+            for j in -imax:imax
+                x=dx*i
+                y=dy*j
+                c=complex(x,y)
+                z=0.0im
+                flag=0
+                for l in 1:200
+                    z=z^iter+c
+                    if abs(z)>2.0
+                        flag=1
+                        break
+                    end
                 end
-            end
-            if flag==0
-                cnt+=1
-                result[:,cnt]=[x,y]
+                if flag==0
+                    cnt+=1
+                    result[iter,:,cnt]=[x,y]
+                end
             end
         end
     end
@@ -30,8 +32,10 @@ end
 
 result = calc()
 
-scatter(result[1,:], result[2,:],
-    markerstrokewidth=0.01,
-    markersize=0.1
-    )
-savefig("manderblot_square.png")
+for iter in 1:5
+    scatter(result[iter,1,:], result[iter,2,:],
+        markerstrokewidth=0.01,
+        markersize=0.1
+        )
+    savefig("./out/manderblot_p$iter.png")
+end
