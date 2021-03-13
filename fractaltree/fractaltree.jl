cb8693581d3c9e8e2a74eb38c7697fc5d44a1d4b
nagamachia <7518545@alumni.tus.ac.jp>
2021-03-13 18:53:21 +0900

    refactor and remove label

--- fractaltree/fractaltree.jl
+++ fractaltree/fractaltree.jl
@@ -1,6 +1,5 @@
 using Plots
 
-L=100.0
 N=50000; c1=0; c2=0; c3=0; c4=0; c5=0; c6=0
 
 # パラメータ
@@ -13,29 +12,29 @@ result[:,1] = [x,y]
 
 function calc(x,y,result,c1,c2,c3,c4,c5,c6)
     for i in 2:N+1
-        r = rand()*L
+        r = rand(Vector(1:10))
         # global x,y,result,c1,c2,c3,c4,c5,c6
-        if r>=0.0 && r<=10.0
+        if r==1
             x=a[1]*x
             y=a[2]*y
             result[:,i] = [x,y]
             c1+=1
-        elseif r>=10.0 && r<=20.0
+        elseif r==2
             x=a[3]*x
             y=a[4]*y+a[5]
             result[:,i] = [x,y]
             c2+=1
-        elseif r>=20.0 && r<=40.0
+        elseif r==3 || r==4
             x=a[6]*x+a[7]*y
             y=a[8]*x+a[9]*y+a[10]
             result[:,i] = [x,y]
             c3+=1
-        elseif r>=40.0 && r<=60.0
+        elseif r==5 || r==6
             x=a[11]*x+a[12]*y
             y=a[13]*x+a[14]*y+a[15]
             result[:,i] = [x,y]
             c4+=1
-        elseif r>=60.0 && r<=80.0
+        elseif r==7 || r==8
             x=a[16]*x+a[17]*y
             y=a[18]*x+a[19]*y+a[20]
             result[:,i] = [x,y]
@@ -56,7 +55,8 @@ println([c1,c2,c3,c4,c5,c6])
 
 scatter(result[1, :], result[2, :],
     markerstrokewidth=0.01,
-    markersize=0.1
+    markersize=0.1,
+    label=""
     )
 savefig("fractaltree.png")
 
