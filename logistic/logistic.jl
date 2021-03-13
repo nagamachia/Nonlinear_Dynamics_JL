1db6d605ebc7fad1dc4c676ea0d5b9f9b95c29e4
nagamachia <7518545@alumni.tus.ac.jp>
2021-03-13 18:54:10 +0900

    added plot using Plots

--- logistic/logistic.jl
+++ logistic/logistic.jl
@@ -1,20 +1,18 @@
 using Pkg
-Pkg.add("GR")
+# Pkg.add("GR")
 # Pkg.add("Plots")
-Pkg.add("Gnuplot")
-Pkg.add("CPUTime")
-Pkg.add("DynamicalSystems")
-Pkg.add("PyPlot")
-# using Plots
-using Gnuplot, LinearAlgebra
+# Pkg.add("CPUTime")
+# Pkg.add("DynamicalSystems")
+# Pkg.add("PyPlot")
+using LinearAlgebra
 using CPUTime
 using DynamicalSystems
-using PyPlot
+using Plots
+# using PyPlot
 
 ds = Systems.logistic()
 i = 1
 pvalues = 3:0.001:4
-ics = [rand() for m in 1:10]
 n = 2000
 Ttr = 2000
 p_index = 1
@@ -28,10 +26,28 @@ for j in 1:L
     y[(1 + (j-1)*n):j*n] .= output[j]
 end
 
-figure()
-PyPlot.title("total points: $(L*n)")
-plot(x, y, ls = "None", ms = 0.5, color = "black", marker = "o", alpha = 0.05)
-xlim(pvalues[1], pvalues[end]); ylim(0,1)
-xlabel("\$r\$"); ylabel("\$x\$")
-tight_layout()
-savefig("logistic.png")
+function plotPyplot()
+    figure()
+    PyPlot.title("total points: $(L*n)")
+    plot(x, y, ls = "None", ms = 0.5, color = "black", marker = "o", alpha = 0.05)
+    xlim(pvalues[1], pvalues[end]); ylim(0,1)
+    xlabel("\$r\$"); ylabel("\$x\$")
+    tight_layout()
+    savefig("logistic.png")
+end
+
+function plotPlots()
+    scatter(x,y,
+    markershape=:circle,
+    markersize=0.5,
+    markeralpha=0.05,
+    title="total points: $(L*n)",
+    xlims=(pvalues[1],pvalues[end]),ylims=(0,1),
+    label="",
+    xlabel="\$r\$",
+    ylabel="\$x\$"
+    )
+    savefig("logisticplots.png")
+end
+
+plotPlots()
