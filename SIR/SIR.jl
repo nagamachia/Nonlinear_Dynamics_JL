cad04835c239a0dc669555c09fbb8b8cf416de65
nagamachia <7518545@alumni.tus.ac.jp>
2021-06-03 00:55:16 +0900

    added failure case using plots

--- SIR/SIR.jl
+++ SIR/SIR.jl
@@ -1,5 +1,6 @@
 using DifferentialEquations
 using CPUTime
+# using Plots
 using PyCall,PyPlot
 
 sir_ode = @ode_def SIRModel begin
@@ -15,8 +16,9 @@ sir_prob = ODEProblem(sir_ode,init,tspan,parms)
 
 @CPUtime sir_sol = solve(sir_prob,saveat = 0.1);
 
-rc("font", family="Segoe UI Emoji")
 # plot(sir_sol,xlabel="Time",ylabel="Number")
+
+rc("font", family="Segoe UI Emoji")
 plot(sir_sol.t,sir_sol[1,:],lw=1,label="💪😀")
 plot(sir_sol.t,sir_sol[2,:],lw=1,label="🦠🤧")
 plot(sir_sol.t,sir_sol[3,:],lw=1,label="💉😷")
@@ -24,4 +26,4 @@ xlabel("Time")
 ylabel("Number")
 legend()
 
-savefig("SIR.svg")
+savefig("SIR.png")
